00439ba6db58a2bdd22f0526af1cc3680b6972a7
Selim Efe <uysalselimefe@gmail.com>
2026-01-18 23:01:54 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,19 @@
 
 int main()
 {
-       int sayilar[]= { 20,60,45,42,23,24,26,125,66,55,145,50,30,40 };
-    int i,j;
+    int sayilar[] = {20, 60, 45, 42, 23, 24, 26, 125, 66, 55, 145, 50, 30, 40};
+    int i, j;
 
-    for(i=0;i<=13;i++){
-        if(sayilar[i]%2==0){
-            sayilar[i]=sayilar[i]+1;
+    for (i = 0; i <= 13; i++)
+    {
+        if (sayilar[i] % 2 == 0)
+        {
+            sayilar[i] = sayilar[i] + 1;
         }
     }
-    for(j=0;j<=13;j++){
-        printf("%d ",sayilar[j]);
+    for (j = 0; j <= 13; j++)
+    {
+        printf("%d ", sayilar[j]);
     }
     return 0;
 }
